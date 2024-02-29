86eafe5af28deacc917832c651128c0621d65ca0
Hmilner1 <72082549+Hmilner1@users.noreply.github.com>
2024-02-29 17:49:22 +0000

    Rabbit Controller Added

---
+++ Source/VRARRabbit/Rabbit_AiController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Rabbit_AiController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRARRABBIT_API ARabbit_AiController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
